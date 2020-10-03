9bc46f241edc3520761c8728d458637d975bbf11
Denis Vlaskin <denis@Mac-mini-Denis.lan>
2020-10-03 20:11:09 +0400

    class User

---
+++ LocationTracker/Model/User.swift
@@ -0,0 +1,25 @@
+//
+//  User.swift
+//  LocationTracker
+//
+//  Created by Denis Vlaskin on 03.10.2020.
+//  Copyright © 2020 Denis Vlaskin. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+final class User: Object {
+    @objc dynamic var login: String = ""
+    @objc dynamic var password: String = ""
+
+    convenience init(_login: String, _password: String) {
+        self.init()
+        self.login = _login
+        self.password = _password
+    }
+
+    override class func primaryKey() -> String? {
+        return "login"
+    }
+}
